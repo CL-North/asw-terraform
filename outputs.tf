9cbdf6e96b94bca10d31badac1978ab98d5b73f4
Ali Chitsazan <105880291+CL-North@users.noreply.github.com>
2022-07-24 19:44:22 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,14 @@ output "s3_bucket_name" {
 }
 
 output "s3_bucket_region" {
-    value = aws_s3_bucket.my-s3-bucket.region
+  value = aws_s3_bucket.my-s3-bucket.region
 }
+
+output "s3_bucket_domain_name" {
+  value = aws_s3_bucket.my-s3-bucket.bucket_domain_name
+}
+
+output "s3_bucket_zone_id" {
+  value = aws_s3_bucket.my-s3-bucket.hosted_zone_id 
+}
+
