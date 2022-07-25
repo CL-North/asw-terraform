537a5127a6c9a5578779d8d7c67d78eb62419b64
Ali Chitsazan <105880291+CL-North@users.noreply.github.com>
2022-07-24 20:43:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,36 +14,11 @@ resource "aws_s3_bucket_versioning" "s3-versioning" {
   }
 }
 
-/* We will define bucket policy here
+/* S3 Bucket policy 
 resource "aws_s3_bucket_policy" "s3-policy" {
   bucket = aws_s3_bucket.my-s3-bucket.id
   policy = policy.json
 }
 */
   
-/* S3 Bucket Permissions [Terraform state is written to the key path/to/my/key]
-
-terraform {
-  backend "s3" {
-    bucket = "my-s3-bucket"
-    key    = "path/to/my/key"
-    region = var.aws_region
-  }
-}
-
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListBucket",
-      "Resource": "arn:aws:s3:::my-s3-bucket"
-    },
-    {
-      "Effect": "Allow",
-      "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
-      "Resource": "arn:aws:s3:::my-s3-bucket/path/to/my/key"
-    }
-  ]
-}
 
